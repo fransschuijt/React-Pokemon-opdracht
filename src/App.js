0ed7741f11bb85a6f98cd9ba80b08d1fd4760b81
fransschuijt <fp.schuijt@gmail.com>
2023-04-11 15:55:56 +0200

    opdracht af

--- src/App.js
+++ src/App.js
@@ -1,12 +1,64 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Pokemon from './components/pokemon/Pokemon';
+import Button from './components/button/Button';
+import logo from './assets/logo.png';
 import './App.css';
 
 function App() {
-  return (
-    <div>
-      Begin hier met de opdracht!
-    </div>
-  );
+    const [pokemons, setPokemons] = useState([]);
+    const [endpoint, setEndpoint] = useState('https://pokeapi.co/api/v2/pokemon/');
+    const [loading, toggleLoading] = useState(false);
+    const [error, setError] = useState(false);
+
+    useEffect(() => {
+        async function fetchData() {
+            toggleLoading(true);
+            setError(false);
+
+            try {
+                const { data } = await axios.get(endpoint);
+                setPokemons(data);
+            } catch(e) {
+                console.error(e);
+                setError(true);
+            }
+
+            toggleLoading(false);
+        }
+
+        fetchData();
+    }, [endpoint]);
+
+    return (
+        <div className="poke-deck">
+            {pokemons &&
+                <>
+                    <img alt="logo" width="400px" src={logo} />
+                    <section className="button-bar">
+                        <Button
+                            disabled={!pokemons.previous}
+                            clickHandler={() => setEndpoint(pokemons.previous)}
+                        >
+                            Vorige
+                        </Button>
+                        <Button
+                            disabled={!pokemons.next}
+                            clickHandler={() => setEndpoint(pokemons.next)}
+                        >
+                            Volgende
+                        </Button>
+                    </section>
+
+                    {pokemons.results && pokemons.results.map((pokemon) => {
+                        return <Pokemon key={pokemon.name} endpoint={pokemon.url} />
+                    })}
+                </>
+            }
+            {loading && <p>Loading...</p>}
+            {error && <p>Er ging iets mis bij het ophalen van de data...</p>}
+        </div>
+    );
 }
 
 export default App;
